5834f0d0f8bc79f7af2b6845dcc93b8642133db0
AMARTYA <amartyadey24@gmail.com>
2023-09-22 15:53:04 +0530

    connected Mysql and added functionality to fectch contact page data to the db

--- dates_data.py
+++ dates_data.py
@@ -1,12 +1,11 @@
 #This module contain all the data related to date and time
-
-import datetime
+from datetime import datetime, date
 
 class Dates_data:
 
     def get_exp_months ():
         #defining experience months
-        current_date = datetime.date.today()
+        current_date = datetime.today()
         current_month = current_date.month
         exp_months = 9
         exp_months += current_month-9
@@ -15,8 +14,11 @@ class Dates_data:
     
     def get_age ():
         # automating age calculation
-        birth_date = datetime.date(2000, 9, 8)
-        current_date = datetime.date.today()
+        birth_date = date(2000, 9, 8)
+        current_date = date.today()
         age = current_date.year - birth_date.year - ((current_date.month, current_date.day) < (birth_date.month, birth_date.day))
 
         return int(age)
+    
+    def get_submit_datetime():
+        return datetime.now()
--- main.py
+++ main.py
@@ -1,8 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from dates_data import Dates_data
-
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+#connecting to Xaamp Mysql through sqlalchemy
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:@localhost/portfolio_amartya"
+db = SQLAlchemy(app)
+
+#class contacts help to update the contacts_form table with relevalent data
+class Contacts(db.Model):
+    '''
+    sno, name, email, subject, message, datetime
+    '''
+    __tablename__ = 'contacts_form' 
+
+    sno = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(20), nullable=False)
+    subject = db.Column(db.String(50), nullable=False)
+    message = db.Column(db.String(200), nullable=False)
+    datetime = db.Column(db.String(10), nullable=True)
+
+    def __init__(self, name, email, subject, message, datetime):
+        self.name = name
+        self.email = email
+        self.subject = subject
+        self.message = message
+        self.datetime = datetime
+
 
 @app.route("/home")
 def home():
@@ -22,8 +48,20 @@ def resume():
 def experiences():
     return render_template('experiences.html')
 
-@app.route("/contact")
+@app.route("/contact", methods = ['GET', 'POST'])
 def contact():
+    if (request.method == 'POST'):
+        #Add data to db
+        name_post = request.form.get('name')
+        email_post = request.form.get('email')
+        subject_post = request.form.get('subject')
+        message_post = request.form.get('message')
+
+        entry = Contacts(name = name_post, email = email_post, subject = subject_post, message = message_post, datetime = Dates_data.get_submit_datetime)
+        db.session.add(entry)
+        db.session.commit()
+    
+
     return render_template('contact.html')
 
 app.run(debug=True)
